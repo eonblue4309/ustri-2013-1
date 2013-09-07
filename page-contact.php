19d1cb603d897196e9950471dd2c2dbb304fa1c3
eonblue4309 <jrrussell1224@gmail.com>
2013-09-07 00:01:27 -0400

    added wufoo contact form
    using wufoo for contact form. still need to test that out and discuss
    what this form is exactly for.

--- page-contact.php
+++ page-contact.php
@@ -12,6 +12,27 @@
   
     <?php the_content(); ?>
 
+    <div id="wufoo-z7x4m1">
+      Fill out my <a href="http://ustamiyaryu.wufoo.com/forms/z7x4m1">online form</a>.
+    </div>
+    <script type="text/javascript">
+      var z7x4m1;(function(d, t) {
+      var s = d.createElement(t), options = {
+      'userName':'ustamiyaryu', 
+      'formHash':'z7x4m1', 
+      'autoResize':true,
+      'height':'908',
+      'async':true,
+      'header':'show'};
+      s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'wufoo.com/scripts/embed/form.js';
+      s.onload = s.onreadystatechange = function() {
+      var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+      try { z7x4m1 = new WufooForm();z7x4m1.initialize(options);z7x4m1.display(); } catch (e) {}};
+      var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+      })(document, 'script');
+    </script>
+
+
   </div>
 
 </section>
